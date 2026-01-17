101332a5ba60c9f04e8e1d499cfd7e60d5916826
icefields <arctic.icefields@slmails.com>
2026-01-17 17:58:35 -0500

    1.01-87 added warning dialog for custom download location

--- app/src/main/java/luci/sixsixsix/powerampache2/presentation/screens/settings/components/ChooseCustomDirDownloads.kt
+++ app/src/main/java/luci/sixsixsix/powerampache2/presentation/screens/settings/components/ChooseCustomDirDownloads.kt
@@ -1,6 +1,28 @@
+/**
+ * Copyright (C) 2025  Antonio Tari
+ *
+ * This file is a part of Power Ampache 2
+ * Ampache Android client application
+ * @author Antonio Tari
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package luci.sixsixsix.powerampache2.presentation.screens.settings.components
 
 import android.net.Uri
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,14 +31,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.OpenInNew
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import luci.sixsixsix.powerampache2.R
 import luci.sixsixsix.powerampache2.common.getCustomDirPermission
 import luci.sixsixsix.powerampache2.presentation.common.TextWithSubtitle
 
@@ -27,11 +59,16 @@ fun ChooseCustomDirDownloads(
     onClearCustomDirDownloads: () -> Unit
 ) {
     val folderLauncher = LocalContext.current.getCustomDirPermission(onFolderSelected)
-    ChooseCustomDirDownloadsContent(
-        currentFolderUriStr = currentFolderUriStr,
-        onFolderSelectButtonClick = { folderLauncher.launch(null) },
-        onClearCustomDirDownloads = onClearCustomDirDownloads
-    )
+    Column(modifier = Modifier.fillMaxWidth()) {
+        ChooseCustomDirDownloadsContent(
+            currentFolderUriStr = currentFolderUriStr,
+            onFolderSelectButtonClick = { folderLauncher.launch(null) },
+            onClearCustomDirDownloads = onClearCustomDirDownloads
+        )
+        if (currentFolderUriStr?.isNotBlank() == true) {
+            ChooseCustomDirWarningMessage()
+        }
+    }
 }
 
 @Composable
@@ -48,16 +85,16 @@ private fun ChooseCustomDirDownloadsContent(
             OutlinedButton(
                 //modifier = Modifier.fillMaxWidth(0.5f),
                 onClick = onClearCustomDirDownloads
-            ) {
-                Text("Reset")
-            }
+            ) { Text(stringResource(R.string.settings_customFolder_reset)) }
             Spacer(modifier = Modifier.height(4.dp).width(8.dp))
         }
 
         TextWithSubtitle(
             modifier = Modifier.weight(1f),
-            title = "Choose custom folder for offline music",
-            subtitle = "Current selected folder: ${currentFolderUriStr(currentFolderUriStr)}",
+            // "Choose custom folder for offline music"
+            title = stringResource(R.string.settings_customFolder_title),
+            //"Current selected folder: ${currentFolderUriStr(currentFolderUriStr)}",
+            subtitle = stringResource(R.string.settings_customFolder_subtitle, currentFolderUriStr(currentFolderUriStr)),
             trailingIcon = Icons.AutoMirrored.Outlined.OpenInNew,
             onClick = onFolderSelectButtonClick
         )
@@ -77,12 +114,60 @@ private fun currentFolderUriStr(currentFolderUriStr: String?): String {
         ?: noSelectionText
 }
 
+@Composable
+private fun ChooseCustomDirWarningMessage() {
+    var isExpanded by remember { mutableStateOf(false) }
+    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)) {
+        OutlinedButton(
+            onClick = { isExpanded = !isExpanded },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.errorContainer,
+                contentColor = MaterialTheme.colorScheme.onErrorContainer
+            ),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                ,
+                text = stringResource(R.string.settings_customFolder_warning_title),
+                color = MaterialTheme.colorScheme.onErrorContainer
+            )
+        }
+        if (isExpanded) {
+            ChooseCustomDirWarningDialog {
+                isExpanded = false
+            }
+        }
+    }
+}
+
+@Composable
+private fun ChooseCustomDirWarningDialog(onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        text = {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(R.string.settings_customFolder_warning_descr),
+                //fontSize = 12.sp,
+                //lineHeight = 12.sp
+            )
+        },
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text(stringResource(android.R.string.ok))
+            }
+        }
+    )
+}
+
 @Preview
 @Composable
 fun ChooseCustomDirDownloadsPreview() {
-    ChooseCustomDirDownloadsContent(
+    ChooseCustomDirDownloads(
         currentFolderUriStr = "content://com.android.externalstorage.documents/tree/Music",
-        onFolderSelectButtonClick = {},
+        onFolderSelected = {},
         onClearCustomDirDownloads = {}
     )
 }
